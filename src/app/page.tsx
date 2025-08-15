10276b010a029098091fae734981c53ca6156f5e
JD2244 <jaydhola246@gmail.com>
2025-08-15 19:39:28 +0530

    02: customization

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,29 @@
+import { Button } from "@/components/ui/button";
+import { Checkbox } from "@/components/ui/checkbox";
+import { Input } from "@/components/ui/input";
+import { Progress } from "@/components/ui/progress";
+import { Textarea } from "@/components/ui/textarea";
 
 export default function Home() {
   return (
-    <div>
-     <p className="text-rose-500">hello world</p>
+    <div className="p-4">
+      <div className="flex flex-col gap-y-4">
+        <div>
+          <Button variant="elevated">hello</Button>
+        </div>
+        <div>
+          <Input placeholder="Hey" />
+        </div>
+        <div>
+          <Progress value={50} />
+        </div>
+        <div>
+          <Textarea placeholder="Hey Hello" />
+        </div>
+        <div>
+          <Checkbox />
+        </div>
+      </div>
     </div>
   );
 }
